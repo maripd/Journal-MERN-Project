e2f0f1a2b4d169167a3541e69baf98c5ac04f621
Marissa Dilig <hello.maripd@gmail.com>
2022-12-28 12:07:43 -0600

    deployment edits

--- models/stickers.js
+++ models/stickers.js
@@ -1,11 +1,11 @@
-// const mongoose = require('mongoose')
-// const Schema = mongoose.Schema
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-// const schema = new Schema(
-//   {
-//     name: { type: String, required: true }
-//   },
-//   { timestamps: true }
-// )
+const schema = new Schema(
+  {
+    name: { type: String, required: true }
+  },
+  { timestamps: true }
+)
 
-// module.exports = mongoose.model('Stickers', schema)
+module.exports = mongoose.model('Stickers', schema)
