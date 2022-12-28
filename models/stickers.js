4441b9ad2bfc21ffbd7fbf5ea9460fb4b72491ad
Marissa Dilig <hello.maripd@gmail.com>
2022-12-28 12:54:40 -0600

    sticker model false required

--- models/stickers.js
+++ models/stickers.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema
 
 const schema = new Schema(
   {
-    name: { type: String, required: true }
+    name: { type: String, required: false }
   },
   { timestamps: true }
 )
