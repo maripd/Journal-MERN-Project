8a9bdc654376cfaaffc1bf36e451ee7ad2b72146
Marissa Dilig <hello.maripd@gmail.com>
2022-12-28 12:58:23 -0600

    removed base url journal card

--- client/src/components/JournalCard.js
+++ client/src/components/JournalCard.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import './card.css'
-const BASE_URL = 'https://marinotepad.herokuapp.com'
+// const BASE_URL = 'https://marinotepad.herokuapp.com'
 
 const JournalCard = (props) => {
   const navigate = useNavigate()
 
   const cardHandleClick = () => {
-    navigate(`${BASE_URL}/pageeditor/${props.id}`)
+    navigate(`pageeditor/${props.id}`)
   }
 
   return (
